31fddb8d7349f3152e091050711e57b0e51ba496
chantel-mills <chantel.mills@student.uts.edu.au>
2021-09-11 22:13:45 +1000

    Search Functionality
    Updated Search.java class and Book.java class to fulfil Search functionality

--- ELMS/src/model/Search.java
+++ ELMS/src/model/Search.java
@@ -7,6 +7,8 @@ package model;
 
 // import here
 import java.util.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  *
@@ -15,57 +17,91 @@ import java.util.*;
 public class Search {
     
     // attributes
-    private String searchInput;
-    LinkedList<Book> books = new LinkedList<>(); // Book class
-    // books meant to be an --> observableArrayList
+    ObservableList<Book> books; // Book class
     
-    public Search(String searchInput) {
-        this.searchInput = searchInput; //input of search from user
+    // Constructors
+    public Search(ObservableList<Book> inputBooks) {
+        books = inputBooks;
     }
     
-    // userInput for search
-    public String userInput() {
-        Scanner sc = new Scanner(System.in);
-        searchInput = sc.nextLine();
-        return searchInput;
-    }
+    // Methods
+    // userInput for search - not needed since data from controller/view
+//    public String userInput() {
+//        Scanner sc = new Scanner(System.in);
+//        searchInput = sc.nextLine(); // need lines 30-31 to be changed to the text box from controller
+//        return searchInput;
+//    }
     
     
-    public LinkedList<String> findBook() { // The type isn't correct I think
+    public ObservableList<Book> byTitle(String title) {
         // loop over database (list of books) // return books that match
-        LinkedList<String> matches = new LinkedList<>();
+        ObservableList<Book> matches = FXCollections.observableArrayList(); // please check
         
-        for (String bookMatch : books) {
-            if (bookMatch.contains(userInput())) {
+        for (Book bookMatch : books) {
+            if (bookMatch.getTitle().toLowerCase().contains(title.toLowerCase())) {
                 matches.add(bookMatch);
             }
         }
         return matches;
     }
     
-    // shows whole list of books available when the "search" menu is open
-    public void showAllBooks() {
-        // show all books on opening
-        for (Book book : books) {
-            System.out.println(book);
+//    // shows whole list of books available when the "search" menu is open
+//    public void showAllBooks() {
+//        // show all books on opening
+//        for (Book book : books) {
+//            System.out.println(book);
+//        }
+//    }
+    
+    public ObservableList<Book> byCategory(String category) {
+//         IDs contain one letter (J,B,A,P) followed by 4 digit number / eg. J0001, B1234
+//         J-journal, B-book, A-article, P-research paper
+        ObservableList<Book> matches = FXCollections.observableArrayList(); // please check!
+        
+        for (Book bookMatch : books) {
+            if (bookMatch.getIsbn().toLowerCase().contains(category.toLowerCase())) {
+                matches.add(bookMatch);
+            }
         }
+        return matches;
+    }
+    
+    public ObservableList<Book> byAuthor(String author) {
+        // loop over database (list of books) // return books that match
+        ObservableList<Book> matches = FXCollections.observableArrayList(); // please check!
+        
+        for (Book bookMatch : books) {
+            if (bookMatch.getAuthor().toLowerCase().contains(author.toLowerCase())) {
+                matches.add(bookMatch);
+            }
+        }
+        return matches;
+    }
+    
+    public ObservableList<Book> byAll(String category, String input) {
+        // loop over database (list of books) // return books that match
+        ObservableList<Book> matches = FXCollections.observableArrayList(); // please check!
+        
+        for (Book bookMatch : books) {
+            if (bookMatch.getIsbn().toLowerCase().contains(category.toLowerCase())) {
+                if (bookMatch.getAuthor().toLowerCase().contains(input.toLowerCase())) {
+                    matches.add(bookMatch);
+                }
+                else {
+                    if (bookMatch.getTitle().toLowerCase().contains(input.toLowerCase())) {
+                        matches.add(bookMatch);
+                    }
+                }
+            }
+        }
+        return matches;
     }
     
     // show all books searched for
-    public void showSearchedBook() {
-        for (String book : findBook()) {
-            System.out.println(book);
-        }
-    }
-    
-//    public void/char identifierBooks() {
-        // switch statement?
-        // IDs contain one letter (J,B,A,P) followed by 4 digit number
-        // J-journal, B-book, A-article, P-research paper
-        // if (/**/.contains('J') { display all ID's with 'J' }
+//    public void showSearchedBook() {
+//        for (Book book : byTitle()) {
+//            System.out.println(book);
+//        }
 //    }
     
-    
-
 }
-
