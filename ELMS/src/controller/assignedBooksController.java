e41bc468d52c8064f47256169f81f43dc45d5962
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2021-09-11 21:25:53 +0800

    Create assignedBooksController.java

---
+++ ELMS/src/controller/assignedBooksController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import au.edu.uts.ap.javafx.Controller;
+import au.edu.uts.ap.javafx.ViewLoader;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.ELMS;
+
+public class assignedBooksController extends Controller<ELMS> {    
+    public assignedBooksController() {
+        //
+    }
+    
+    @FXML public void handleExitBtn(ActionEvent e) { Platform.exit(); }
+    @FXML public void handleReturnBtn(ActionEvent e) throws IOException { 
+        ViewLoader.showStage(getELMS(), "/view/Account.fxml", this.stage.getTitle(), this.stage);
+    }
