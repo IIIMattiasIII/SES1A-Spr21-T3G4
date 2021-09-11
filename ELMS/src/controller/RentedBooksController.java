2c5aa36216e0be6c62c9fb22a3a7d9d6640c1752
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2021-09-11 21:18:04 +0800

    Create RentedBooksController.java

---
+++ ELMS/src/controller/RentedBooksController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import au.edu.uts.ap.javafx.Controller;
+import au.edu.uts.ap.javafx.ViewLoader;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.ELMS;
+
+public class RentedBooksController extends Controller<ELMS> {    
+    public RentedBooksController() {
+        //
+    }
