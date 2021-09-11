3e611c82c6760da1b09edae0c5f785bcd801f462
Mattias <88617353+IIIMattiasIII@users.noreply.github.com>
2021-09-12 02:14:14 +1000

    Added functionality for reading and writing accounts to csv and added demo accounts csv

--- ELMS/src/model/ELMS.java
+++ ELMS/src/model/ELMS.java
@@ -1,5 +1,15 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -11,8 +21,8 @@ public class ELMS {
 
     public ELMS() {
         userSelected = false;
-        addAccount(0, "admin", "", "password", 0);
-        importBooks();
+        importAccounts();
+//        importBooks();
     }
     
     private void importBooks() {
@@ -20,7 +30,22 @@ public class ELMS {
     }
     
     private void importAccounts() {
-        // TBD: add code to populate accounts list from users csv sheet
+        BufferedReader csvReader;
+        try {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("data/usersdemo.csv");
+            csvReader = new BufferedReader(new InputStreamReader(inputStream));
+            String row;
+            while ((row = csvReader.readLine()) != null) {
+                String[] data = row.split(",");
+                accounts.add(new Account(Integer.parseInt(data[0]), data[1], data[2], data[3], Integer.parseInt(data[4])));
+            }
+            csvReader.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ELMS.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ELMS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
     public ObservableList<Account> getAccounts() {
@@ -28,8 +53,27 @@ public class ELMS {
     }
     
     public void addAccount(int ID, String nameF, String nameS, String pass, int permLvl) {
-        accounts.add(new Account(ID, nameF, nameS, pass, permLvl));
-        // TBD: Add integration with accounts csv
+        FileWriter csvWriter;
+        try {
+            final String dir = System.getProperty("user.dir");
+            csvWriter = new FileWriter(dir+"/src/data/usersdemo.csv", true); // Not sure this is best way to get the source - may need to be modified
+            csvWriter.append(""+ID);
+            csvWriter.append(",");
+            csvWriter.append(nameF);
+            csvWriter.append(",");
+            csvWriter.append(nameS);
+            csvWriter.append(",");
+            csvWriter.append(pass);
+            csvWriter.append(",");
+            csvWriter.append(""+permLvl);
+            csvWriter.append("\n");
+            csvWriter.close();
+            accounts.add(new Account(ID, nameF, nameS, pass, permLvl));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ELMS.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ELMS.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public Account getSelectedAccount() {
