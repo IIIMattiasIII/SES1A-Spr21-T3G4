5e28371c354933ec4377c93c032cd9f5f4263766
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2021-09-12 09:41:06 +0800

    Update RentedBooksController.java

--- ELMS/src/controller/RentedBooksController.java
+++ ELMS/src/controller/RentedBooksController.java
@@ -2,6 +2,8 @@ package controller;
 
 import au.edu.uts.ap.javafx.Controller;
 import au.edu.uts.ap.javafx.ViewLoader;
+import java.io.IOException;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -17,8 +19,9 @@ public class RentedBooksController extends Controller<ELMS> {
        @FXML private Button handleReturnBtn;
         //
     }
-    
+    public final ELMS getELMS() { return model; }
     @FXML public void handleExitBtn(ActionEvent e) { Platform.exit(); }
     @FXML public void handleReturnBtn(ActionEvent e) throws IOException { 
         ViewLoader.showStage(getELMS(), "/view/Account.fxml", this.stage.getTitle(), this.stage);
     }
+}
