901b09d1122e4dd18108598f0e88ebc5384d9cd8
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2021-09-12 23:05:58 +0800

    Commit(12/09/21)

--- ELMS/src/controller/ViewAccountController.java
+++ ELMS/src/controller/ViewAccountController.java
@@ -3,24 +3,48 @@ package controller;
 import au.edu.uts.ap.javafx.*;
 import java.io.IOException;
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
+import javafx.scene.control.TableView;
+import model.Book;
 import model.ELMS;
 
 public class ViewAccountController extends Controller<ELMS> {    
     public ViewAccountController() {
         //
     }
+    @FXML private TableView<Book> RentBookTable;
+    @FXML private TableView<Book> rentingHistoryTable;
+    @FXML private TableView<Book> assignedBooksTable;
+    @FXML private TableView<Book> mainTable;
+    @FXML private TableView<Book> finesTable;
 
     public final ELMS getELMS() { return model; }
-    
-    @FXML public void handleLibBtn(ActionEvent e) throws IOException {
-        ViewLoader.showStage(getELMS(), "/view/LibraryMenu.fxml", this.stage.getTitle(), this.stage);
+    /*titleClmn.setCellValueFactory(cellData -> cellData.getValue().titleProperty());
+    authorClmn.setCellValueFactory(cellData -> cellData.getValue().authorProperty());
+    stockClmn.setCellValueFactory(cellData -> cellData.getValue().stockProperty());
+    IDClmn.setCellValueFactory(cellData -> cellData.getValue().IDProperty());*/
+    @FXML public void handleRentBooksBtn(ActionEvent e) throws IOException {
+        /*for (Book book : model.getBooks()) {
+        }
+        mainTable = RentBookTable;
+        mainTable.setItems((ObservableList<Book>) RentBookTable);*/
+    }
+    @FXML public void handleAssignedBooksBtn(ActionEvent e) throws IOException {
+        /*mainTable = assignedBooksTable;
+        mainTable.setItems((ObservableList<Book>) assignedBooksTable);*/
+    }
+    @FXML public void handleFinesBtn(ActionEvent e) throws IOException {
+        /*mainTable  = finesTable;
+        mainTable.setItems((ObservableList<Book>) finesTable);*/
+    }
+    @FXML public void handleRentHistBtn(ActionEvent e) throws IOException {
+        /*mainTable = rentingHistoryTable;
+        mainTable.setItems((ObservableList<Book>) rentingHistoryTable);*/
     }
     
-    @FXML public void handleAccBtn(ActionEvent e) throws IOException {
-        ViewLoader.showStage(getELMS(), "/view/Account.fxml", this.stage.getTitle(), this.stage);
-    }
+    //@FXML private TableView<Book> Table;
     
     @FXML public void handleExitBtn(ActionEvent e) { Platform.exit(); }
     @FXML public void handleReturnBtn(ActionEvent e) throws IOException { 
--- ELMS/src/model/Book.java
+++ ELMS/src/model/Book.java
@@ -2,13 +2,20 @@ package model;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 
 public class Book {
-    private final StringProperty title = new SimpleStringProperty();
-    // TBD: Add other attributes
+    public final StringProperty title = new SimpleStringProperty();
+    public final StringProperty author = new SimpleStringProperty();
+    public final IntegerProperty stock = new SimpleIntegerProperty();
+    public final IntegerProperty ID = new SimpleIntegerProperty();
     
-    public Book(String title) {
+    public Book(String title, String author, int stock, int ID) {
         this.title.set(title);
+        this.author.set(author);
+        this.stock.set(stock);
+        this.ID.set(ID);
         // TBD: Add other attributes in constructor
     }
     
@@ -16,5 +23,16 @@ public class Book {
     public void setTitle(String title) { this.title.set(title); }
     public StringProperty titleProperty() { return this.title; }
     
+    public String getAuthor() {return this.author.get(); }
+    public void setAuthor(String author) {this.author.set(author); }
+    public StringProperty authorProperty() { return this.author; }
+    
+    public int getStock() { return this.stock.get(); }
+    public void setStock(int stock) { this.stock.set(stock); }
+    public IntegerProperty stockProperty() {return this.stock; }
+    
+    public int getID() { return this.ID.get(); }
+    public void setID(int ID) { this.ID.set(ID); }
+    public IntegerProperty IDProperty() {return this.ID; }
     // TBD: Add other getters and setters for other attribute properties
 }
