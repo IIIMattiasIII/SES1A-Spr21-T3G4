c0076ea0deb45bb74f358577fd0c10185dccbdd6
Mattias <88617353+IIIMattiasIII@users.noreply.github.com>
2021-09-13 14:28:53 +1000

    Revert "Merge pull request #1 from IIIMattiasIII/MohammadKarim01-SearchFunction"
    This reverts commit 2dd87e7db7eb553e0768a8098bbf87e2eb7e624e, reversing
    changes made to b369d2b8d6bbb90a826c64ae2f694224f120205d.

--- ELMS/src/model/Book.java
+++ ELMS/src/model/Book.java
@@ -5,35 +5,16 @@ import javafx.beans.property.StringProperty;
 
 public class Book {
     private final StringProperty title = new SimpleStringProperty();
-    private String isbn; // copied from 'Bookshell' branch
-    private String author; // copied from 'Bookshell' branch
     // TBD: Add other attributes
     
-    public Book(String title, String id, String author) {
+    public Book(String title) {
         this.title.set(title);
-        this.isbn = id; // copied from 'Bookshell' branch
-        this.author = author; // copied from 'Bookshell' branch
         // TBD: Add other attributes in constructor
     }
     
     public String getTitle() { return this.title.get(); }
     public void setTitle(String title) { this.title.set(title); }
     public StringProperty titleProperty() { return this.title; }
-    
-    
-    public String getIsbn() { // method copied from 'Bookshell' branch
-        return isbn;
-    }
-    public void setIsbn(String isbn) { // method copied from 'Bookshell' branch
-        this.isbn = isbn;
-    }
-    
-    public String getAuthor() { // method copied from 'Bookshell' branch
-        return isbn;
-    }
-    public void setAuthor(String isbn) { // method copied from 'Bookshell' branch
-        this.isbn = isbn;
-    }
     
     // TBD: Add other getters and setters for other attribute properties
 }
