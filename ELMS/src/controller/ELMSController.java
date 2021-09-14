3dd930fc404d00c4fb42cdb58b6b3f39b90889d2
Mattias <88617353+IIIMattiasIII@users.noreply.github.com>
2021-09-14 13:10:06 +1000

    Added interfaces and controllers for accessing admin options

--- ELMS/src/controller/ELMSController.java
+++ ELMS/src/controller/ELMSController.java
@@ -5,11 +5,33 @@ import java.io.IOException;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
+import javafx.scene.control.Button;
 import model.ELMS;
 
 public class ELMSController extends Controller<ELMS> {    
+    @FXML private Button adminBtn;
+    @FXML private Button staffBtn;
+    
     public ELMSController() {
         //
+    }
+    
+    @FXML public void initialize() {
+        switch (getELMS().getSelectedAccount().getPermissionLevel()) {
+            case 0:
+                adminBtn.setDisable(false);
+                staffBtn.setDisable(false);
+                break;
+            case 1:
+                adminBtn.setVisible(false);
+                staffBtn.setDisable(false);
+                break;
+            default:
+                adminBtn.setVisible(false);
+                staffBtn.setVisible(false);
+                break;
+        }
+        
     }
 
     public final ELMS getELMS() { return model; }
@@ -20,6 +42,14 @@ public class ELMSController extends Controller<ELMS> {
     
     @FXML public void handleAccBtn(ActionEvent e) throws IOException {
         ViewLoader.showStage(getELMS(), "/view/ViewAccount.fxml", this.stage.getTitle(), this.stage);
+    }
+    
+    @FXML public void handleAdminBtn(ActionEvent e) throws IOException {
+        ViewLoader.showStage(getELMS(), "/view/AdminMenu.fxml", this.stage.getTitle(), this.stage);
+    }
+    
+    @FXML public void handleStaffBtn(ActionEvent e) throws IOException {
+        ViewLoader.showStage(getELMS(), "/view/StaffMenu.fxml", this.stage.getTitle(), this.stage);
     }
     
     @FXML public void handleExitBtn(ActionEvent e) { Platform.exit(); }
