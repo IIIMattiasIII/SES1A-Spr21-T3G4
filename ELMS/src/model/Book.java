133ae1a5814887cd8d9f2f23f5183e4ccfca0da8
Mattias <88617353+IIIMattiasIII@users.noreply.github.com>
2021-09-14 14:02:02 +1000

    Added demo book list and read+write code for books csv

--- ELMS/src/model/Book.java
+++ ELMS/src/model/Book.java
@@ -1,20 +1,45 @@
 package model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Book {
+    private final StringProperty id = new SimpleStringProperty();
     private final StringProperty title = new SimpleStringProperty();
-    // TBD: Add other attributes
+    private final StringProperty author = new SimpleStringProperty();
+    private final StringProperty genre = new SimpleStringProperty();
+    private final IntegerProperty stock = new SimpleIntegerProperty();
     
-    public Book(String title) {
+    public Book(String ID, String title, String author, String genre, int stock) {
+        this.id.set(ID);
         this.title.set(title);
-        // TBD: Add other attributes in constructor
+        this.author.set(author);
+        this.genre.set(genre);
+        this.stock.set(stock);
+        
     }
+    
+    public String getID() { return this.id.get(); }
+    public void setID(String id) { this.id.set(id); }
+    public StringProperty idProperty() { return this.id; }
     
     public String getTitle() { return this.title.get(); }
     public void setTitle(String title) { this.title.set(title); }
     public StringProperty titleProperty() { return this.title; }
     
-    // TBD: Add other getters and setters for other attribute properties
+    public String getAuthor() { return this.author.get(); }
+    public void setAuthor(String author) { this.author.set(author); }
+    public StringProperty authorProperty() { return this.author; }
+    
+    public String getGenre() { return this.genre.get(); }
+    public void setGenre(String genre) { this.genre.set(genre); }
+    public StringProperty genreProperty() { return this.genre; }
+    
+    public int getStock() { return this.stock.get(); }
+    public void setStock(String stock) { this.title.set(stock); }
+    public IntegerProperty stockProperty() { return this.stock; }
+    
+
 }
--- ELMS/src/model/ELMS.java
+++ ELMS/src/model/ELMS.java
@@ -1,5 +1,13 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -16,7 +24,22 @@ public class ELMS {
     }
     
     private void importBooks() {
-        // TBD: add code to populate books list from books csv sheet
+        BufferedReader csvReader;
+        try {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("data/books.csv");
+            csvReader = new BufferedReader(new InputStreamReader(inputStream));
+            String row;
+            while ((row = csvReader.readLine()) != null) {
+                String[] data = row.split(",");
+//                System.out.println(data[0]+", "+data[1]+", "+data[2]+", "+data[3]+", "+data[4]);
+                books.add(new Book(data[0], data[1], data[2], data[3], Integer.parseInt(data[4])));
+            }
+            csvReader.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ELMS.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ELMS.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     private void importAccounts() {
@@ -29,6 +52,30 @@ public class ELMS {
     
     public void addAccount(int ID, String nameF, String nameS, String pass, int permLvl) {
         accounts.add(new Account(ID, nameF, nameS, pass, permLvl));
+    }
+    
+    public void addBook(int ID, String title, String author, String genre, int stock) {
+        FileWriter csvWriter;
+        try {
+            final String dir = System.getProperty("user.dir");
+            csvWriter = new FileWriter(dir+"/src/data/books.csv", true); // Not sure this is best way to get the source - may need to be modified
+            csvWriter.append(""+ID);
+            csvWriter.append(",");
+            csvWriter.append(title);
+            csvWriter.append(",");
+            csvWriter.append(author);
+            csvWriter.append(",");
+            csvWriter.append(genre);
+            csvWriter.append(",");
+            csvWriter.append(""+stock);
+            csvWriter.append("\n");
+            csvWriter.close();
+            accounts.add(new Account(ID, title, author, genre, stock));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ELMS.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ELMS.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public Account getSelectedAccount() {
