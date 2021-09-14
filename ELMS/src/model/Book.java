9a0232734452eed8e350c4ec31c4dbec38c605cf
Mattias <88617353+IIIMattiasIII@users.noreply.github.com>
2021-09-14 14:02:02 +1000

    Cherry picked Books addition from main
    Picked from 133ae1a5814887cd8d9f2f23f5183e4ccfca0da8

--- ELMS/src/model/Book.java
+++ ELMS/src/model/Book.java
@@ -6,33 +6,37 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
 public class Book {
-    public final StringProperty title = new SimpleStringProperty();
-    public final StringProperty author = new SimpleStringProperty();
-    public final IntegerProperty stock = new SimpleIntegerProperty();
-    public final IntegerProperty ID = new SimpleIntegerProperty();
+    private final StringProperty id = new SimpleStringProperty();
+    private final StringProperty title = new SimpleStringProperty();
+    private final StringProperty author = new SimpleStringProperty();
+    private final StringProperty genre = new SimpleStringProperty();
+    private final IntegerProperty stock = new SimpleIntegerProperty();
     
-    public Book(String title, String author, int stock, int ID) {
+    public Book(String ID, String title, String author, String genre, int stock) {
+        this.id.set(ID);
         this.title.set(title);
         this.author.set(author);
+        this.genre.set(genre);
         this.stock.set(stock);
-        this.ID.set(ID);
-        // TBD: Add other attributes in constructor
     }
+    
+    public String getID() { return this.id.get(); }
+    public void setID(String id) { this.id.set(id); }
+    public StringProperty idProperty() { return this.id; }
     
     public String getTitle() { return this.title.get(); }
     public void setTitle(String title) { this.title.set(title); }
     public StringProperty titleProperty() { return this.title; }
     
-    public String getAuthor() {return this.author.get(); }
-    public void setAuthor(String author) {this.author.set(author); }
+    public String getAuthor() { return this.author.get(); }
+    public void setAuthor(String author) { this.author.set(author); }
     public StringProperty authorProperty() { return this.author; }
     
-    public int getStock() { return this.stock.get(); }
-    public void setStock(int stock) { this.stock.set(stock); }
-    public IntegerProperty stockProperty() {return this.stock; }
+    public String getGenre() { return this.genre.get(); }
+    public void setGenre(String genre) { this.genre.set(genre); }
+    public StringProperty genreProperty() { return this.genre; }
     
-    public int getID() { return this.ID.get(); }
-    public void setID(int ID) { this.ID.set(ID); }
-    public IntegerProperty IDProperty() {return this.ID; }
-    // TBD: Add other getters and setters for other attribute properties
+    public int getStock() { return this.stock.get(); }
+    public void setStock(String stock) { this.title.set(stock); }
+    public IntegerProperty stockProperty() { return this.stock; }
 }
