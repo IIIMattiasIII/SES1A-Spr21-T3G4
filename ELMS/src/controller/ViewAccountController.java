cdb7c3be6ce13a8d534d0fa3ec6f5f6093b3be1f
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2021-10-02 10:37:32 +0800

    Update 3
    Update 3

--- ELMS/src/controller/ViewAccountController.java
+++ ELMS/src/controller/ViewAccountController.java
@@ -14,11 +14,7 @@ public class ViewAccountController extends Controller<ELMS> {
     public ViewAccountController() {
         //
     }
-    @FXML private TableView<Book> RentBookTable;
-    @FXML private TableView<Book> rentingHistoryTable;
-    @FXML private TableView<Book> assignedBooksTable;
     @FXML private TableView<Book> mainTable;
-    @FXML private TableView<Book> finesTable;
 
     public final ELMS getELMS() { return model; }
     /*titleClmn.setCellValueFactory(cellData -> cellData.getValue().titleProperty());
@@ -26,25 +22,13 @@ public class ViewAccountController extends Controller<ELMS> {
     stockClmn.setCellValueFactory(cellData -> cellData.getValue().stockProperty());
     IDClmn.setCellValueFactory(cellData -> cellData.getValue().IDProperty());*/
     @FXML public void handleRentBooksBtn(ActionEvent e) throws IOException {
-        /*for (Book book : model.getBooks()) {
-        }
-        mainTable = RentBookTable;
-        mainTable.setItems((ObservableList<Book>) RentBookTable);*/
     }
     @FXML public void handleAssignedBooksBtn(ActionEvent e) throws IOException {
-        /*mainTable = assignedBooksTable;
-        mainTable.setItems((ObservableList<Book>) assignedBooksTable);*/
     }
     @FXML public void handleFinesBtn(ActionEvent e) throws IOException {
-        /*mainTable  = finesTable;
-        mainTable.setItems((ObservableList<Book>) finesTable);*/
     }
     @FXML public void handleRentHistBtn(ActionEvent e) throws IOException {
-        /*mainTable = rentingHistoryTable;
-        mainTable.setItems((ObservableList<Book>) rentingHistoryTable);*/
     }
-    
-    //@FXML private TableView<Book> Table;
     
     @FXML public void handleExitBtn(ActionEvent e) { Platform.exit(); }
     @FXML public void handleReturnBtn(ActionEvent e) throws IOException { 
