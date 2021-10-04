1abd4115ddca57126616b0be5aa2315e5e1b657b
Mattias <88617353+IIIMattiasIII@users.noreply.github.com>
2021-10-04 16:19:34 +1100

    (NF) Revert Controller changes in UTS provided package
    This file should not need to be edited
    NF: Commit is non-functional

--- ELMS/src/au/edu/uts/ap/javafx/Controller.java
+++ ELMS/src/au/edu/uts/ap/javafx/Controller.java
@@ -1,24 +1,8 @@
 package au.edu.uts.ap.javafx;
 
-import java.io.IOException;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 import javafx.stage.*;
-import model.*;
 
 public abstract class Controller<M> {
     protected M model;
     protected Stage stage;
-    @FXML public ListView  List;
-    public Search search = new Search();
-    public Borrowings borrowings= new Borrowings();
-  @FXML public void availableList(){
-    
-    List.setItems(search.getAvailablebooks());
-}
-  @FXML public void resetList(ActionEvent event)throws IOException{
-    
-    List.setItems(search.getAvailablebooks());
-}
 }
